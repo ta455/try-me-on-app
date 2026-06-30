fcf7a42c5003ee25eb169963a42ca47a01bcd9ed
manita <manita@gmail.com>
2026-06-30 20:42:50 +0700

    fix: remove Github Pages base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/try-me-on-app/",
+  plugins: [react()],  
 });
